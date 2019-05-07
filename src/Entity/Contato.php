42d44b5cf0a422077ff626b465320457493ed55c
David Alves <david2000tsi@gmail.com>
2019-05-07 19:50:34 -0300

    Added entities and configured database connection (test only)

---
+++ src/Entity/Contato.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContatoRepository")
+ */
+class Contato
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $telefone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getTelefone(): ?int
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(int $telefone): self
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+}
